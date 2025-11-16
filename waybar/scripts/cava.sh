5c9d2df65df10c39ab10a96dcc2fcdde36c830a6
Sam <0000114513595xsp@al.educacao.sp.gov.br>
2025-11-16 16:48:28 -0300

    Add files via upload

--- waybar/scripts/cava.sh
+++ waybar/scripts/cava.sh
@@ -1,4 +1,29 @@
-#!/bin/bash
+#! /bin/bash
 
-cava -p ~/.config/cava/config_waybar | sed -u 's/;//g;s/0/▁/g;s/1/▂/g;s/2/▃/g;s/3/▄/g;s/4/▅/g;s/5/▆/g;s/6/▇/g;s/7/█/g;'
+bar="▁▂▃▄▅▆▇█"
+dict="s/;//g;"
 
+# creating "dictionary" to replace char with bar
+i=0
+while [ $i -lt ${#bar} ]; do
+  dict="${dict}s/$i/${bar:$i:1}/g;"
+  i=$((i = i + 1))
+done
+
+# write cava config
+config_file="/tmp/polybar_cava_config"
+echo "
+[general]
+bars = 18
+
+[output]
+method = raw
+raw_target = /dev/stdout
+data_format = ascii
+ascii_max_range = 7
+" >$config_file
+
+# read stdout from cava
+cava -p $config_file | while read -r line; do
+  echo $line | sed $dict
+done
